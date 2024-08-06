5256db3ade033aaaf5afa76ccb0350244997a7f4
it <kyle9742@naver.com>
2024-08-06 16:07:24 +0900

    searchbar에 입력한 값 데이터 api로 가져오기

---
+++ src/main/resources/static/js/Character.js
@@ -0,0 +1,23 @@
+const accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDA1MTM1MDEifQ.QB7_7pNIV2NvmPskoESH-rN5d7UDd0rToWwnyeJIIau6PpixyG1kVFVlmF8uaKz4XS_wLEj0eEs7ZgYkceFdHicYREOmrcjshtZ12diZt45sYSEaWTIe_fVgYew3DBBvamX02T7iLgRn7iJVqunMKM3SkEDzruKPIVVb_kw4L5d-wgp-W-IJD5MZ3OYyDgWI4x4SLNbEIQ_1RrmgWXgZAlCHJS--Y5C0dErPqmabCxwmUSlS0oSGQcSWRvYVhQDYAJIxmetWwkOI8uOSz3bZMuXNiBFH5BXrHWoasxfKQmzU9HrsLLePxsxVPpvqiE1yKO3D-fapHL7lBiohGYieMA"
+
+async function getCharacterList(searchText) {
+
+    let basicURL = `https://developer-lostark.game.onstove.com/characters`
+
+    if (searchText !== undefined) {
+        basicURL += "/" + searchText + "/siblings";
+    } else {
+        return "/characterlist";
+    }
+
+    console.log(basicURL);
+
+    const result = await axios.get(basicURL,{
+
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    });
+
+    return result.data;
+}
