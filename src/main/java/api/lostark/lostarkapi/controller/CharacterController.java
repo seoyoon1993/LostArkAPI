117181e78e0f6508cafb52e11abd2a3d4d76ef43
it <kyle9742@naver.com>
2024-08-08 09:27:22 +0900

    character 페이지 컨트롤러 수정

--- src/main/java/api/lostark/lostarkapi/controller/CharacterController.java
+++ src/main/java/api/lostark/lostarkapi/controller/CharacterController.java
@@ -3,7 +3,9 @@ package api.lostark.lostarkapi.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
@@ -17,9 +19,10 @@ public class CharacterController {
         return "/character/characterlist";
     }
 
-    @GetMapping("/character")
+    @GetMapping("/{characterName}")
     @PreAuthorize("hasRole('USER')")
-    public String character() {
+    public String character(@PathVariable String characterName, Model model) {
+        model.addAttribute("characterName", characterName);
         return "/character/character";
     }
 }
