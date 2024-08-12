ed4a1e62962c7eaa60d0fe7f6404254cc320ede6
it <kyle9742@naver.com>
2024-08-12 13:06:09 +0900

    08 / 12

--- src/main/resources/static/js/Character.js
+++ src/main/resources/static/js/Character.js
@@ -1,12 +1,12 @@
 const accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDA1MTM1MDEifQ.QB7_7pNIV2NvmPskoESH-rN5d7UDd0rToWwnyeJIIau6PpixyG1kVFVlmF8uaKz4XS_wLEj0eEs7ZgYkceFdHicYREOmrcjshtZ12diZt45sYSEaWTIe_fVgYew3DBBvamX02T7iLgRn7iJVqunMKM3SkEDzruKPIVVb_kw4L5d-wgp-W-IJD5MZ3OYyDgWI4x4SLNbEIQ_1RrmgWXgZAlCHJS--Y5C0dErPqmabCxwmUSlS0oSGQcSWRvYVhQDYAJIxmetWwkOI8uOSz3bZMuXNiBFH5BXrHWoasxfKQmzU9HrsLLePxsxVPpvqiE1yKO3D-fapHL7lBiohGYieMA"
 
 const className = ["디스트로이어", "워로드", "버서커", "홀리나이트", "슬레이어", "스트라이커", "브레이커", "배틀마스터", "인파이터",
-                            "기공사", "창술사", "데빌헌터", "블래스터", "호크아이", "스카우터", "건슬링어", "바드", "서머너", "아르카나", "소서리스", "블레이드",
-                            "데모닉", "리퍼", "소울이터", "도화가", "기상술사"];
+    "기공사", "창술사", "데빌헌터", "블래스터", "호크아이", "스카우터", "건슬링어", "바드", "서머너", "아르카나", "소서리스", "블레이드",
+    "데모닉", "리퍼", "소울이터", "도화가", "기상술사"];
 
 const classIcon = ["destroyer", "warlord", "berserker", "holyknight", "berserker_female", "striker", "infighter_male", "battlemaster", "infighter", "soulmaster",
-                            "lancemaster", "devilhunter", "blaster", "hawkeye", "scouter", "gunslinger", "bard", "summoner", "arcana", "sorceress", "blade", "demonic",
-                            "reaper", "soul_eater", "artist", "weather_artist"];
+    "lancemaster", "devilhunter", "blaster", "hawkeye", "scouter", "gunslinger", "bard", "summoner", "arcana", "sorceress", "blade", "demonic",
+    "reaper", "soul_eater", "artist", "weather_artist"];
 
 const iconUrl = "https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/";
 
@@ -32,137 +32,15 @@ async function getCharacterList(searchText) {
     return resultList;
 }
 
-async function getCharacterProfile(characterName) {
+async function getCharacter(characterName) {
+    let characterURL = "https://developer-lostark.game.onstove.com/armories/characters/" + characterName
 
-    let basicURL = "https://developer-lostark.game.onstove.com/armories/characters/" + characterName + "/profiles"
-
-    const result = await axios.get(basicURL, {
-
+    const result = await axios.get(characterURL, {
         headers: {
             Authorization: `Bearer ${accessToken}`
         }
-    });
+    })
 
-    console.log(result.data)
-    return result.data;
-}
-
-async function getCharacterEquipment(characterName) {
-
-    let basicURL = "https://developer-lostark.game.onstove.com/armories/characters/" + characterName + "/equipment"
-
-    const result = await axios.get(basicURL, {
-
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-
-    console.log(result.data)
-    return result.data;
-}
-
-async function getCharacterAvatars(characterName) {
-
-    let basicURL = "https://developer-lostark.game.onstove.com/armories/characters/" + characterName + "/avatars"
-
-    const result = await axios.get(basicURL, {
-
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-
-    console.log(result.data)
-    return result.data;
-}
-
-async function getCharacterCombatskills(characterName) {
-
-    let basicURL = "https://developer-lostark.game.onstove.com/armories/characters/" + characterName + "/combat-skills"
-
-    const result = await axios.get(basicURL, {
-
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-
-    console.log(result.data)
-    return result.data;
-}
-
-async function getCharacterEngravings(characterName) {
-
-    let basicURL = "https://developer-lostark.game.onstove.com/armories/characters/" + characterName + "/engravings"
-
-    const result = await axios.get(basicURL, {
-
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-
-    console.log(result.data)
-    return result.data;
-}
-
-async function getCharacterCards(characterName) {
-
-    let basicURL = "https://developer-lostark.game.onstove.com/armories/characters/" + characterName + "/cards"
-
-    const result = await axios.get(basicURL, {
-
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-
-    console.log(result.data)
-    return result.data;
-}
-
-async function getCharacterGems(characterName) {
-
-    let basicURL = "https://developer-lostark.game.onstove.com/armories/characters/" + characterName + "/gems"
-
-    const result = await axios.get(basicURL, {
-
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-
-    console.log(result.data)
-    return result.data;
-}
-
-async function getCharacterColosseums(characterName) {
-
-    let basicURL = "https://developer-lostark.game.onstove.com/armories/characters/" + characterName + "/colosseums"
-
-    const result = await axios.get(basicURL, {
-
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-
-    console.log(result.data)
-    return result.data;
-}
-
-async function getCharacterCollectibles(characterName) {
-
-    let basicURL = "https://developer-lostark.game.onstove.com/armories/characters/" + characterName + "/collectibles"
-
-    const result = await axios.get(basicURL, {
-
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-
-    console.log(result.data)
+    console.log(result.data);
     return result.data;
 }
